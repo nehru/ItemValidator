53f9c38a730894d9eac632f217deae30729162ec
nehrusathappan@gmail.com <nehrusathappan@gmail.com>
2023-05-05 16:34:28 -0700

    Added 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjE5OTQxOQ/spectral.js?branch_slug=main&token=193aa9e6-1268-41f2-af88-805c3ad2788f";
+export default { extends: ruleset };
